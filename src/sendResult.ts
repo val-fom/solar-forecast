6d75e139118ac0bc2e4ef3bd691546ab1994090e
Valentyn Fomenko <fomenko.vn@gmail.com>
2025-09-28 16:37:42 +0300

    fix: update sendResult function to accept any type for text parameter

--- src/sendResult.ts
+++ src/sendResult.ts
@@ -2,7 +2,7 @@ import config from "./config";
 
 const { TELEGRAM_TOKEN, TELEGRAM_CHAT_ID } = config;
 
-export async function sendResult(text: string): Promise<any> {
+export async function sendResult(text: any): Promise<any> {
   if (typeof text !== "string") {
     text = JSON.stringify(text, null, 2);
   }
