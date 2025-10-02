9c3622e971c736f91756c55dc538cb7b4bacd756
Valentyn Fomenko <valentyn4menko@gmail.com>
2025-10-02 23:21:17 +0300

    refactor: rename run function to morning and update serverless configuration

--- handler.ts
+++ handler.ts
@@ -2,7 +2,11 @@ import { getForecast } from "./src/getForecast";
 import { sendResult } from "./src/sendResult";
 import { getDevicesStats } from "./src/getDevicesStats";
 
-export async function run(): Promise<void> {
+export async function morning(): Promise<void> {
+  await sendResult("#forecast", await getForecast());
+}
+
+export async function evening(): Promise<void> {
   await sendResult("#forecast", await getForecast());
   await sendResult("#mppt_totals", (await getDevicesStats()).totals);
 }
