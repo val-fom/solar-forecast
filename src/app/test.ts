caf64fae61800e9fccdc3ea7272c579e12171441
Valentyn Fomenko <valentyn4menko@gmail.com>
2026-01-19 21:00:32 +0200

    refactor: simplify getOperationLog query parameters and remove defaults from test function

--- src/app/test.ts
+++ src/app/test.ts
@@ -7,9 +7,6 @@ export const test = async () => {
   const deviceOperationLog = await getOperationLog(TUYA_DEVICE_ID_TEST, {
     start_time: +getMonthStartDate(),
     end_time: Date.now(),
-    query_type: 1,
-    size: 100, // max 100
-    type: '7',
   })
   console.log(JSON.stringify(deviceOperationLog, null, 2))
   return deviceOperationLog
--- src/integrations/tuya/devices/getOperationLog.ts
+++ src/integrations/tuya/devices/getOperationLog.ts
@@ -5,13 +5,18 @@ export type OperationLogResult = Record<string, unknown>
 
 export async function getOperationLog(
   deviceId: string,
-  query?: Record<string, string | number | undefined>,
+  query?: { start_time: number; end_time: number },
 ): Promise<OperationLogResult> {
   const { access_token } = await getTuyaToken()
 
   return callTuya<OperationLogResult>({
     path: `/v2.0/cloud/thing/${deviceId}/logs`,
     accessToken: access_token,
-    query,
+    query: {
+      query_type: 1,
+      size: 100, // max 100
+      type: '7',
+      ...query,
+    },
   })
 }
